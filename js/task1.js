1c8c6d6a483624d46452459b0fe84b07ce924c84
Makc <mks59k@gmail.com>
2019-01-03 00:19:31 +0200

    Реализовано задание 1

--- js/task1.js
+++ js/task1.js
@@ -1,3 +1,19 @@
-function checkName() {
+// Вариант 1
+function checkName(event) {
     
+    var key = event.keyCode;
+
+    if (key >= 48 && key <= 57){
+        event.preventDefault();
+    }
 }
+
+
+// Вариант 2
+// inputName.onkeypress = function(event){
+//     var key = event.keyCode;
+
+//     if (key >= 48 && key <= 57){
+//         return false;
+//     }
+// }
